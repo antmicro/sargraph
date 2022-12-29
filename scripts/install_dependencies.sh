38b0c0abff3bde7f51a7eb4b34fc1e47731d11fd
Marcelina Oset <moset@internships.antmicro.com>
2022-12-29 11:33:54 +0100

    [#39898] .ci: Added installing git

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt -qqy update > /dev/null
-apt -qqy install --no-install-recommends screen sysstat gnuplot-nox python3 python3-pip stress coreutils > /dev/null
+apt -qqy install --no-install-recommends screen sysstat gnuplot-nox python3 python3-pip git stress coreutils > /dev/null
 pip install -r requirements.txt
 pip install git+https://github.com/antmicro/servis
 ln -s $(realpath sargraph.py) /usr/bin/sargraph
