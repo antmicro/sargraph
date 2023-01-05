f613d202fe71618c2880c82645eb0db8f9dfa040
Marcelina Oset <moset@internships.antmicro.com>
2023-01-05 17:20:31 +0100

    [#40458] scripts: install dependencies: Removed installing Servis twice

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,5 +3,4 @@
 apt -qqy update > /dev/null
 apt -qqy install --no-install-recommends screen sysstat gnuplot-nox python3 python3-pip git stress coreutils > /dev/null
 pip install -r requirements.txt
-pip install git+https://github.com/antmicro/servis
 ln -s $(realpath sargraph.py) /usr/bin/sargraph
